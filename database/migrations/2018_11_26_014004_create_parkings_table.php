33fafa0e8d30fcec89452d0a83e5baa9bc2926b6
PrimeAndrew <raparicio71@gmail.com>
2018-11-26 01:39:40 -0400

    migrations laravel

--- database/migrations/2018_11_26_014004_create_parkings_table.php
+++ database/migrations/2018_11_26_014004_create_parkings_table.php
@@ -15,7 +15,17 @@ class CreateParkingsTable extends Migration
     {
         Schema::create('parkings', function (Blueprint $table) {
             $table->increments('id');
+            //$table->unsignedInteger('id_zone_fk');
+            //$table->unsignedInteger('id_price_list_fk');
+            $table->string('parking_name',45);
+            $table->string('parking_address',150);
+            $table->unsignedInteger('total_spaces');
+            $table->time('open_hour');
+            $table->time('close_hour');
+            $table->decimal('latitude',10,7);
+            $table->decimal('longitud',10,7);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
